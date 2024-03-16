aea98d64ed4b2f08880fe47f0939f20bc70f257d
AdamAitOufkir <adamoufkir05@gmail.com>
2024-03-16 22:04:32 +0000

    sidebar + dark mode

--- frontend/authapp/src/app/components/nav/nav.component.ts
+++ frontend/authapp/src/app/components/nav/nav.component.ts
@@ -9,12 +9,24 @@ import { Emitters } from '../../emiters/emitters';
 })
 export class NavComponent {
   authenticated = false
-
+  fullname = ""
+  email = ""
   constructor(private http:HttpClient){}
   ngOnInit(): void {
     Emitters.authEmitter.subscribe((auth:boolean)=>{
       this.authenticated = auth
     })
+    this.http.get('http://localhost:2000/api/user',{
+      withCredentials:true
+    })
+    .subscribe(
+      (res:any) => {
+        this.fullname = res.fullname
+        this.email = res.email
+    },
+    (err) => {
+    }
+    );
   }
 
   logout(){
